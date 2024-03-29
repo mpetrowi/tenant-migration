fbdf47af642439ea530566b435554c94d1ed7c81
Matt Petro <matt.petro@atomicjolt.com>
2024-03-29 09:12:04 -0400

    Add migration generation

---
+++ analyze_schema.rb
@@ -0,0 +1,70 @@
+#!/bin/ruby
+
+# Usage: analayze_schema rails_dir
+#
+#  To make the schema.rb:
+#    bundle exec rake db:fix_structure
+#     -> db/schema.rb
+#
+# Migrates apartment data to the public schema, transforming all tenanted primary keys
+
+require 'active_support/inflector'
+require 'zlib'
+
+path = ARGV[0] || '.'
+
+init = File.open(path + "/config/initializers/apartment.rb").read
+if !File.file?(path + "/db/schema.rb")
+  puts "Missing schema, run  bundle exec rake db:fix_structure"
+  exit
+end
+
+schema = File.open(path + "/db/schema.rb").read
+
+global_models = init.split("\n").select do |line|
+  line.include?("config.excluded_models")..line.include?("}") ? true : false
+end
+global_models = (global_models[1..-2]).map(&:strip)
+global_tables = global_models.map { |t| ActiveSupport::Inflector.tableize(t).tr('/', '_') }
+
+puts <<EOF
+class AddTenantId < ActiveRecord::Migration[7.0]
+  def change
+    tenant_id = TenantMigrationSupport::current_apartment_tenant&.id
+
+    opts = {
+      foreign_key: { to_table: "public.tenants", on_delete: :cascade },
+      default: tenant_id,
+      null: tenant_id.nil?,
+    }
+
+EOF
+
+table = ''
+schema.split("\n").grep(/create_table|t\.index .* unique: true/) do |line|
+  if m = line.match(/create_table "(?<table>[^"]*)"/)
+    table = m[:table]
+    if !global_tables.include?(table)
+      puts <<EOF
+
+    # #{table}
+    add_column :#{table}, :tenant_id, :bigint, **opts
+    add_index :#{table}, :tenant_id
+EOF
+    end
+  elsif !global_tables.include?(table)
+    m = line.match(/t\.index (?<cols>\[.*\]), name: "(?<name>[^"]*)", unique: true(?<rest>.*)$/)
+    raise "invalid: #{line}" if !m
+
+    puts <<EOF
+    remove_index :#{table}, name: #{m[:name]}
+    add_index :#{table}, #{m[:cols]}, unique: true#{m[:rest]}
+EOF
+  end
+end
+
+puts <<EOF
+  end
+end
+EOF
+
